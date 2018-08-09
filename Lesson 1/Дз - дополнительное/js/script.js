89d56baaf5880cd8d44723126a0290b0a4efba64
salikhov <shamil644666@gmail.com>
2018-08-09 21:33:26 +0300

    3 коммит

---
+++ Lesson 1/Дз - дополнительное/js/script.js
@@ -0,0 +1,13 @@
+var num = 33721;
+
+var factor = 3*3*7*2*1; 
+
+console.log(factor);
+
+var degree = Math.pow(factor, 3);
+
+console.log(degree);
+
+var str = String(degree);
+
+alert(str.substr(0,2));
